67612d628da0ae37b9a749ad71be0bca2bfcafd9
Mike Medvedev <mmedvedev20@gmail.com>
2024-12-15 21:44:41 -0500

    added search bar that filters items in list

--- src/public/components/data.js
+++ src/public/components/data.js
@@ -4,26 +4,35 @@ const data = {
   Guild: ["Starfire II", "X-500"],
 };
 
-const aside = document.getElementById("layout-aside");
-
-for (const [brand, model] of Object.entries(data)) {
-  const sec = document.createElement("section");
-  aside.appendChild(sec);
-  console.log(sec);
-  sec.innerText = brand;
-  const list = document.createElement("div");
-  list.id = "guitarList";
-  sec.appendChild(list);
-  model.forEach((m) => {
-    const li = document.createElement("span");
-    li.id = "guitarItem";
-    li.innerText = m;
-    list.appendChild(li);
-  });
+const container = document.getElementById("listContainer");
+function renderList(list = data) {
+  for (const [brand, model] of Object.entries(list)) {
+    const sec = document.createElement("section");
+    container.appendChild(sec);
+    sec.innerText = brand;
+    const list = document.createElement("div");
+    list.id = "guitarList";
+    sec.appendChild(list);
+    model.forEach((m) => {
+      const li = document.createElement("p");
+      li.id = m;
+      li.className = "guitarItem";
+      li.innerText = m;
+      list.appendChild(li);
+    });
+  }
 }
-
-let field;
+renderList();
 const input = document.getElementById("searchInput");
 input.addEventListener("input", (e) => {
-  field = e.target.value;
+  filterList(e.target.value);
 });
+
+function filterList(searchString) {
+  container.innerHTML = "";
+  const filteredData = Object.entries(data).filter(([brand, model], index) =>
+    brand.toLowerCase().includes(searchString.toLowerCase())
+  );
+  const updatedData = Object.fromEntries(filteredData);
+  renderList(updatedData);
+}
